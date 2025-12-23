99bce27d46729a94073b355e1a0ac0fa90a7d9f1
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-23 09:04:29 -0500

    [Auto-Publish] Index: see-life-clearly-why-costa-del-mar-sunglasses-are-a-game-cha

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -2,8 +2,10 @@
 // 🤖 Backend appends to this file when new posts are built
 
 import { post as see_the_world_in_style_kaliyadi_sunglasses_review_for_every_ } from './see-the-world-in-style-kaliyadi-sunglasses-review-for-every-.data'
+import { post as see_life_clearly_why_costa_del_mar_sunglasses_are_a_game_cha } from './see-life-clearly-why-costa-del-mar-sunglasses-are-a-game-cha.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  see_life_clearly_why_costa_del_mar_sunglasses_are_a_game_cha,
   see_the_world_in_style_kaliyadi_sunglasses_review_for_every_
 ]
