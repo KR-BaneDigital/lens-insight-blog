4e8d6c347ee59ec3bce19f4d343bb27c109d5e91
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-23 11:04:02 -0500

    [Auto-Publish] Index: see-life-in-style-why-retro-polarized-sunglasses-are-a-must-

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -4,9 +4,11 @@
 import { post as see_the_world_in_style_kaliyadi_sunglasses_review_for_every_ } from './see-the-world-in-style-kaliyadi-sunglasses-review-for-every-.data'
 import { post as see_life_clearly_why_costa_del_mar_sunglasses_are_a_game_cha } from './see-life-clearly-why-costa-del-mar-sunglasses-are-a-game-cha.data'
 import { post as see_clearly_and_stylishly_the_magic_of_polarized_sunglasses } from './see-clearly-and-stylishly-the-magic-of-polarized-sunglasses.data'
+import { post as see_life_in_style_why_retro_polarized_sunglasses_are_a_must_ } from './see-life-in-style-why-retro-polarized-sunglasses-are-a-must-.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  see_life_in_style_why_retro_polarized_sunglasses_are_a_must_,
   see_clearly_and_stylishly_the_magic_of_polarized_sunglasses,
   see_life_clearly_why_costa_del_mar_sunglasses_are_a_game_cha,
   see_the_world_in_style_kaliyadi_sunglasses_review_for_every_
